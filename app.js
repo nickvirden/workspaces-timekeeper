400a33e9234b02ec6b0e34373c62967b1dd25ec9
Kelvan Ince <incekelvan@gmail.com>
2017-01-21 20:52:55 -0600

    Added IBMs functionality

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var express = require('express'),
   logger = require('morgan'),
   bodyParser = require('body-parser'),
   jwt = require('jwt-express'),
+  crypto = require('crypto'),
   request = require('request');
 
 // Route Files
@@ -62,6 +63,105 @@ var newCard = {
 
 // Trello.post('/cards/', newCard, success, error);
 
+/**
+ * Middleware function to handle the Watson Work challenge
+ */
+function verifier(req, res, next) {
+  if(req.body.type === 'verification') {
+    console.log('Got Webhook verification challenge ' + req.body);
+
+    var bodyToSend = {
+      response: req.body.challenge
+    };
+
+    var hashToSend = crypto.createHmac('sha256', WEBHOOK_SECRET)
+          .update(JSON.stringify(bodyToSend))
+          .digest('hex');
+
+    res.set('X-OUTBOUND-TOKEN', hashToSend);
+    res.send(bodyToSend);
+  } else {
+		next();
+	}
+}
+
+var jwtToken = '';
+var errors = 0;
+
+/**
+ * Obtains the JWT token needed to post messages to spaces.
+ */
+function initialize() {
+	oauth.run(
+		appId,
+		appSecret,
+		(err, token) => {
+			if(err) {
+				console.error(`Failed to get JWT token - attempt ${errors}`);
+				errors++;
+				if(errors > 10) {
+					console.error(`Too many JWT token attempts; giving up`);
+					return;
+				}
+				setTimeout(initialize, 10000);
+				return;
+			}
+
+			console.log("Initialized JWT token");
+			jwtToken = token();
+		});
+}
+
+
+function respond(text, spaceId, callback) {
+	var url = `https://api.watsonwork.ibm.com/v1/spaces/${spaceId}/messages`;
+	var body = {
+		headers: {
+			Authorization: `Bearer ${jwtToken}`
+		},
+		json: true,
+		body: {
+			type: 'appMessage',
+			version: 1.0,
+			annotations: [{
+					type: 'generic',
+					version: 1.0,
+					color: '#6CB7FB',
+					text: text,
+			}]
+		}
+	};
+
+	console.log('Responding to ' + url + ' with ' + JSON.stringify(body));
+
+	request.post(url, body, (err, res) => {
+		if (err || res.statusCode !== 201) {
+			console.error(`Error sending message to ${spaceId} ${err}`);
+			callback(err);
+			return;
+		}
+		callback(null, res.body);
+	});
+}
+
+
+// Authorize application.
+// request(authenticationOptions, function(err, response, body) {
+
+//   // If successful authentication, a 200 response code is returned
+//   if(response.statusCode == 200){
+//     console.log ("Authentication successful\n");
+//     console.log ("App Id: " + authenticationOptions.auth.user);
+//     console.log ("App Secret: " + authenticationOptions.auth.pass + "\n");
+//     console.log ("access_token:\n\n" + JSON.parse(body).access_token + "\n");
+//     console.log ("token_type: " + JSON.parse(body).token_type);
+//     console.log ("expires_in: " + JSON.parse(body).expires_in);
+//     console.log ("\n");
+//   } else {
+//     console.log("Error authenticating with\nApp: " + authenticationOptions.auth.user + "\nSecret: " + authenticationOptions.auth.pass + "\n\n");
+//   }
+// });
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -102,20 +202,3 @@ app.listen(port, hostname, function() {
 });
 
 module.exports = app;
-
-// Authorize application.
-// request(authenticationOptions, function(err, response, body) {
-
-//   // If successful authentication, a 200 response code is returned
-//   if(response.statusCode == 200){
-//     console.log ("Authentication successful\n");
-//     console.log ("App Id: " + authenticationOptions.auth.user);
-//     console.log ("App Secret: " + authenticationOptions.auth.pass + "\n");
-//     console.log ("access_token:\n\n" + JSON.parse(body).access_token + "\n");
-//     console.log ("token_type: " + JSON.parse(body).token_type);
-//     console.log ("expires_in: " + JSON.parse(body).expires_in);
-//     console.log ("\n");
-//   } else {
-//     console.log("Error authenticating with\nApp: " + authenticationOptions.auth.user + "\nSecret: " + authenticationOptions.auth.pass + "\n\n");
-//   }
-// });
